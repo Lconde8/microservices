7bc0c86b04522362a4fbd58c291b2b4a11488ace
Luis Enrique Gomez Conde <142526462+Lconde8@users.noreply.github.com>
2024-02-07 15:26:16 -0600

    Delete shopping directory

--- shopping/src/main/java/com/shopping/shopping/Repository/InvoiceItemsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.shopping.shopping.Repository;
-
-import com.shopping.shopping.Entity.InvoiceItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvoiceItemsRepository extends JpaRepository<InvoiceItem, Long> {
-}
