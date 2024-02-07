a2805270fd054c6609f92ff1c09c2f4a0d160ad8
Luis Enrique <enriqueconde8@hotmail.com>
2024-02-07 15:36:49 -0600

    adding files of the proyect

---
+++ customer/src/main/java/com/costumer/costumer/Repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.costumer.costumer.Repository;
+
+import com.costumer.costumer.Entity.Customer;
+import com.costumer.costumer.Entity.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    public Customer findByNumberID(String numberID);
+    public List<Customer> findByLastName(String lastName);
+    public List<Customer> findByRegion(Region region);
+}
